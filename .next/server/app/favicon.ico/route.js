a06a84f3bdb0b876e1fe5d22d595abfef4dd935a
panda-lsy <shengxia23@hainnu.edu.cn>
2026-01-13 15:57:39 +0800

    chore: 优化 .gitignore，屏蔽输出文件
    - 添加 .spec-workflow/ 到忽略列表
    - 从 Git 跟踪中移除 .next/ 目录下的所有构建产物

--- .next/server/app/favicon.ico/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../chunks/[turbopack]_runtime.js")("server/app/favicon.ico/route.js")
-R.c("server/chunks/[root-of-the-server]__b01ab6e1._.js")
-R.c("server/chunks/[root-of-the-server]__5e0842cd._.js")
-R.c("server/chunks/node_modules_next_b1fdeea4._.js")
-R.c("server/chunks/_next-internal_server_app_favicon_ico_route_actions_353150a5.js")
-R.m(32682)
-module.exports=R.m(32682).exports
--- .next/server/chunks/_next-internal_server_app_api_session_route_actions_30262845.js
+++
@@ -1,3 +0,0 @@
-module.exports=[16915,(e,o,d)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_api_session_route_actions_30262845.js.map
