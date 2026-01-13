f498e6716aca8dcc212eeebbeb6f5364ca72c68e
panda-lsy <shengxia23@hainnu.edu.cn>
2026-01-13 17:13:49 +0800

    chore: remove build artifacts from Git tracking and update .gitignore
    - Remove .next/, out/, .esaconfig/ from Git tracking
    - Update .gitignore to properly ignore build outputs
    - Add .turbo/ and .swc/ cache directories

--- .next/server/app/api/session/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/session/route.js")
-R.c("server/chunks/[root-of-the-server]__a0866aaa._.js")
-R.c("server/chunks/[root-of-the-server]__780a2049._.js")
-R.c("server/chunks/[root-of-the-server]__5e0842cd._.js")
-R.c("server/chunks/_next-internal_server_app_api_session_route_actions_30262845.js")
-R.m(36659)
-module.exports=R.m(36659).exports
